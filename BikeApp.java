0ce04638e7ae8b01f2ef0fb40ebf059a96129b6b
RW0990 <whiteryan2599@icloud.com>
2026-03-07 13:16:37 +0000

    Add files via upload

---
+++ BikeApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package week9.bikeapp;
+
+/**
+ *
+ * @author ryanwhite
+ */
+public class BikeApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ NormalBike.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package week9.bikeapp;
+
+/**
+ *
+ * @author ryanwhite
+ */
+public class NormalBike {
+    
+}
